01472c19ec327de066a19d792828f9e8d809fff0
Kunal Verma <kunalvrm555@gmail.com>
2022-07-10 20:03:43 +0530

    modified hooks.py for WhatsApp client

--- hooks.py
+++ hooks.py
@@ -7,18 +7,6 @@ from erp import req_args
    
 def notices_updated(notices):
     for notice in notices:
-        message = {
-            'to': env['NOTICES_EMAIL_ADDRESS'],
-            'from': 'no-reply@mftp.herokuapp.com',
-            'fromname': 'MFTP',
-            'subject': 'Notice: %s - %s' % (notice['subject'],
-                                            notice['company']),
-        }
-        files = []
-        if 'attachment_url' in notice:
-            filename = "attachment.pdf"
-            files = [('attachment', (filename, notice['attachment_raw']))]
-
         r = requests.post(env['SEND_MESSAGE_URL'],data={
             "message":notice['text'],
         },verify=False)
@@ -26,5 +14,3 @@ def notices_updated(notices):
             r = requests.post(env['SEND_FILE_URL'],files={"attachment":notice['attachment_raw']})
         
         time.sleep(10)
-
-        print ('Sent notice to', message['to'], ':', message['subject'], r.text)
