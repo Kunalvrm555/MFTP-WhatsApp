437cc120ba11e2325533d0a4c1aa0e13ea87d7b2
Kunal Verma <kunalvrm555@gmail.com>
2022-07-11 06:00:37 +0530

    modifications in hooks.py for sending attachment

--- hooks.py
+++ hooks.py
@@ -1,5 +1,6 @@
 from os import environ as env
 import requests
+import base64
 import time
 import settings
 from os import environ as env
@@ -7,10 +8,11 @@ from erp import req_args
    
 def notices_updated(notices):
     for notice in notices:
-        r = requests.post(env['SEND_MESSAGE_URL'],data={
-            "message":notice['text'],
-        },verify=False)
         if 'attachment_url' in notice:
-            r = requests.post(env['SEND_FILE_URL'],files={"attachment":notice['attachment_raw']})
-        
-        time.sleep(10)
+            encoded_string = base64.b64encode(notice['attachment_raw'])
+            r = requests.post(env['SEND_FILE_URL'],data={'base64string':encoded_string},stream=True)
+            print("Attachment sent")
+        r = requests.post(env['SEND_MESSAGE_URL'],data={
+            "message": notice['text']+'\n'+notice['time'],
+        },verify=False)
+        time.sleep(5)
