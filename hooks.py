aefa5f54d79a5d0d9afae3e1e7d4938aabb31244
Kunal Verma <kunalvrm555@gmail.com>
2022-08-01 06:59:42 +0530

    add interval between attachment and text post

--- hooks.py
+++ hooks.py
@@ -8,11 +8,11 @@ from erp import req_args
    
 def notices_updated(notices):
     for notice in notices:
+        r = requests.post(env['SEND_MESSAGE_URL'],data={
+            "message": notice['text']+'\n'+notice['time'],
+        },verify=False)
+        time.sleep(10)
         if ('attachment_url') in notice:
             encoded_string = base64.b64encode(notice['attachment_raw'])
             r = requests.post(env['SEND_FILE_URL'],data={'base64string':encoded_string},stream=True)
             print("Attachment sent")
-        r = requests.post(env['SEND_MESSAGE_URL'],data={
-            "message": notice['text']+'\n'+notice['time'],
-        },verify=False)
-        time.sleep(5)
